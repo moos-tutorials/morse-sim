c1517aff495c0f27b45c1df7b8cccbc6565804e6
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2017-02-23 00:04:18 -0500

    [morse] Better models of the vehicles.

--- models/mokai.py
+++ models/mokai.py
@@ -1,13 +1,35 @@
 from morse.builder import *
 
-robot = Robot('mokai.blend')
+def_freq = 60
+timewarp = 1
+freq = def_freq / timewarp
+
+mokai1 = Robot('mokai.blend')
+mokai1.frequency(freq)
+mokai2 = Robot('mokai.blend')
+mokai2.translate(0,3,0)
+mokai2.frequency(freq)
+
+heron = Robot('heron.blend')
+heron.frequency(freq)
+heron2 = Robot('heron.blend')
+heron.translate(3,0,0)
+heron2.translate(3,3,0)
+heron2.frequency(freq)
 
 
 # print('NoGravity=',robot.get_properties())
 
-env = Environment('pavlab.blend')
+env = Environment('pavlab.blend', fastmode = True)
 # env = Environment('water-1/water_scene')
+
 env.set_camera_clip(clip_end=1000)
+env.set_camera_speed(15.)
 env.set_camera_location([50,0,60])
 env.set_camera_rotation([.7,0,1.57])
 env.set_camera_focal_length(15.0)
+
+env.simulator_frequency(freq)
+env.set_time_scale(timewarp)
+env.use_vsync('OFF')
+env.use_internal_syncer()
