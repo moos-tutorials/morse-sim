4fbc5cb04ae2dc2c4931a86b6f8f679cf19ea260
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2017-02-24 00:12:51 -0500

    [morse] give teleport to heron

--- pavlab_sim/src/pavlab_sim/builder/robots/heron.py
+++ pavlab_sim/src/pavlab_sim/builder/robots/heron.py
@@ -4,7 +4,7 @@ class Heron(Robot):
     """
     A template robot model for heron, with a motion controller and a pose sensor.
     """
-    def __init__(self, name = None, debug = True):
+    def __init__(self, name = None, debug = False):
 
         # heron.blend is located in the data/robots directory
         Robot.__init__(self, 'pavlab_sim/robots/heron.blend', name)
@@ -34,9 +34,14 @@ class Heron(Robot):
         self.pose = Pose()
         self.append(self.pose)
 
+        self.teleport = Teleport()
+        self.append(self.teleport)
+
     def set_moos(self, moos_host='127.0.0.1', moos_port=9000,
                     moos_name='iMorse_heron'):
         self.motion.add_stream('moos',moos_host=moos_host, moos_port=moos_port,
                                 moos_name=moos_name)
         self.pose.add_stream('moos',moos_host=moos_host, moos_port=moos_port,
+                                moos_name=moos_name)
+        self.teleport.add_stream('moos',moos_host=moos_host, moos_port=moos_port,
                                 moos_name=moos_name)
